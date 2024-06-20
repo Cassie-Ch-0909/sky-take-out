04027ac9add2cbd39579b828bfb9150a78d7b509
Cassie-Ch-0909 <cassie20190909@gmail.com>
2024-06-20 17:45:46 +0800

    给员工登录和员工登出的接口添加类、属性、方法的注解，使swagger生成的接口文档更详细易读

--- sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
@@ -7,9 +7,11 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
+// 用在类上，例如entity DTO VO
 @ApiModel(description = "员工登录时传递的数据模型")
 public class EmployeeLoginDTO implements Serializable {
 
+    // 用在属性上，描述属性信息
     @ApiModelProperty("用户名")
     private String username;
 
