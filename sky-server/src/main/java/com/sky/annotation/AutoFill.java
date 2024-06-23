489db6a398fd7c2ef1998dd55b9aa5c4190f53d5
Cassie-Ch-0909 <cassie20190909@gmail.com>
2024-06-24 03:56:03 +0800

    @AutoFill公共字段自动填充

--- sky-server/src/main/java/com/sky/annotation/AutoFill.java
+++ sky-server/src/main/java/com/sky/annotation/AutoFill.java
@@ -1,7 +1,6 @@
 package com.sky.annotation;
 
 import com.sky.enumeration.OperationType;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,7 +9,9 @@ import java.lang.annotation.Target;
 /**
  * 自定义注解，用于标识某个方法需要进行功能字段自动填充处理
  */
+// 自定义注解（即在 @Target 注解之后的注解）可以标注在方法上，而不能标注在其他类型的元素（如类、字段、参数等）上
 @Target(ElementType.METHOD)
+// 自定义注解在运行时也会被保留，并且可以通过反射机制读取到
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AutoFill {
     //数据库操作类型：UPDATE INSERT
--- sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -11,26 +11,38 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
-
 import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 
 /**
- * 自定义切面，实现公共字段自动填充处理逻辑
+ * 自定义切面，实现公共字段自动填充处理逻辑实现思路：
+ * 1. 自定义注解AutoFill,用于标识需要进行公共字段自动填充的方法
+ * 2. 自定义切面类AutoFillAspect, 统一拦截加入了AutoFill注解的方法，通过反射为公共字段赋值
+ * insert时，create_time和create_user需要赋值
+ * insert和update时，create_time create_user update_time update_user需要赋值
+ * 3. 在Mapper方法上加入AutoFill注解
  */
+
+// Spring 框架中用于声明切面的注解
 @Aspect
 @Component
 @Slf4j
 public class AutoFillAspect {
 
     /**
-     * 切入点
+     * 切入点: 明确对哪些类的哪些方法来进行拦截
+     * com.sky.mapper表示com.sky.mapper 包及其所有子包
+     * *.*：前面的*表示所有类，后面的*表示所有方法
+     * com.sky.mapper表示对 com.sky.mapper 包中所有类的所有方法都将应用该切面定义的通知（Advice）逻辑。
+     *
+     * @annotation(com.sky.annotation.AutoFill)：匹配所有被 com.sky.annotation.AutoFill 注解标注的方法
      */
     @Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotation.AutoFill)")
     public void autoFillPointCut(){}
 
     /**
      * 前置通知，在通知中进行公共字段的赋值
+     * 当匹配上autoFillPointCut()切点，则执行该方法
      */
     @Before("autoFillPointCut()")
     public void autoFill(JoinPoint joinPoint){
@@ -47,6 +59,7 @@ public class AutoFillAspect {
             return;
         }
 
+        // 实体类是不确定的，可能是Employee，可能是Category，也可能是其他，所以这边是Object类型
         Object entity = args[0];
 
         //准备赋值的数据
@@ -57,6 +70,10 @@ public class AutoFillAspect {
         if(operationType == OperationType.INSERT){
             //为4个公共字段赋值
             try {
+                /*
+                * 在 entity 的类中查找一个名为 AutoFillConstant.SET_CREATE_TIME（假设其值为 "setCreateTime"）的方法，
+                * 这个方法接受一个 LocalDateTime 类型的参数，并将这个方法对象赋值给 setCreateTime 变量。
+                * */
                 Method setCreateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_TIME, LocalDateTime.class);
                 Method setCreateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_USER, Long.class);
                 Method setUpdateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
